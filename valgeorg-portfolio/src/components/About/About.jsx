50467ad7a33c35b313357d095ff1323bd51c9a29
Valentin Georgiev <valentingeorgiev1997@abv.bg>
2024-02-11 23:13:57 +0200

    Add: About page

--- valgeorg-portfolio/src/components/About/About.jsx
+++ valgeorg-portfolio/src/components/About/About.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
+import aboutMePhoto from "../../assets/climb.png";
+import { FaReact, FaHtml5, FaCss3Alt, FaBootstrap, FaGitAlt } from 'react-icons/fa';
+import { SiTypescript, SiJavascript, SiChakraui } from 'react-icons/si';
+
 
 const About = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
-
-export default About
+    return (
+      <div className="container col-xxl-8 px-4 py-5">
+        <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+          <div className="col-10 col-sm-8 col-lg-6">
+            <img src={aboutMePhoto} className="d-block mx-lg-auto img-fluid rounded" alt="valentin-image" width="400" height="200" loading="lazy"/>
+          </div>
+          <div className="col-lg-6">
+            <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">Valentin Georgiev</h1>
+            <p className="lead">I'm Valentin, an aspiring Frontend Developer who started his Programming Adventure at Telerik Academy an year ago. My passion for web development has led me to acquire extensive knowledge and hands-on experience in JavaScript, React, GIT, CSS, HTML, Chakra UI, TypeScript and little knowledge in NextJS and Tailwind CSS that I am currently enhancing with an exciting project.My journey of continuous learning extends to TypeScript, where I'm actively expanding my knowledge while working on personal projects to enrich my portfolio.My profound interest lies in Software Development, where I bring creativity,proactiveness, a strong desire to learn, and an openness to opportunities as I embark on my journey to begin a career as a Software Developer.</p>
+            <div className="skills-section py-5">
+          <h2 className="fw-bold mb-3">Skills:</h2>
+          <div className="skills-icons d-flex flex-wrap justify-content-start">
+            <div><FaReact /></div>
+            <div><SiTypescript /> </div>
+            <div><FaBootstrap /></div>
+            <div><SiJavascript /> </div>
+            <div><FaHtml5 /> </div>
+            <div><FaCss3Alt /></div>
+            <div><FaGitAlt /></div>
+            <div><SiChakraui /></div>
+          </div>
+        </div>
+          </div>
+        </div>
+        
+      </div>
+    );
+  }
+  
+  export default About;
--- valgeorg-portfolio/src/components/Home/Home.jsx
+++ valgeorg-portfolio/src/components/Home/Home.jsx
@@ -5,15 +5,12 @@ import githubIcon from "../../assets/github.svg"
 
 const Home = () => {
   return (
-    <div className="container col-xxl-8 px-4 py-5">
-    <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
-      <div className="col-10 col-sm-8 col-lg-6">
-        <img src={vg} className="d-block mx-lg-auto img-fluid rounded-circle" alt="valentin-image" width="350" height="150" loading="lazy"/>
-      </div>
-      <div className="col-lg-6">
-        <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">Valentin Georgiev</h1>
-        <p className="lead">Software Developer</p>
-        <div className="d-grid gap-2 d-md-flex justify-content-md-start">
+    <div className="px-4 py-5 my-5 text-center">
+<img className="d-block mx-auto mb-4 rounded-circle" src={vg} alt="hike" />
+<h1 className="display-5 fw-bold text-body-emphasis">Valentin Georgiev</h1>
+<div className="col-lg-6 mx-auto">
+  <p className="lead mb-4">SOFTWARE DEVELOPER</p>
+  <div className="d-grid gap-2 d-md-flex justify-content-md-center">
             <a href="https://www.linkedin.com/in/valentin-georgiev-414950160/" 
                className="btn btn-lg px-4 me-md-2" 
                role="button" 
@@ -27,9 +24,8 @@ const Home = () => {
               <img src={githubIcon} alt="GitHub" height="40" width="40"/>
             </a>
           </div>
-      </div>
-    </div>
-  </div>
+</div>
+</div>
   )
 }
 
