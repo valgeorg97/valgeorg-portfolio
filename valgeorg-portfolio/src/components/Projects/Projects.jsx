363a7029746e8bf5694e1ec44701e057b9480efa
Valentin Georgiev <valentingeorgiev1997@abv.bg>
2024-02-15 20:12:34 +0200

    Add: Style Projects Header

--- valgeorg-portfolio/src/components/Projects/Projects.jsx
+++ valgeorg-portfolio/src/components/Projects/Projects.jsx
@@ -8,18 +8,18 @@ import coming_soon from "../../assets/coming_soon.jpg"
 const Projects = () => {
   return (
     <div class="container">
-      <div class="content-section-heading text-center">
-        <h3 class="text-secondary mb-0">Portfolio</h3>
-        <h2 class="mb-5">Recent Projects</h2>
+      <div className="content-section-heading text-center">
+        <h3 className="text-secondary mb-0">Portfolio</h3>
+        <h2 className="mb-5">Recent Projects</h2>
       </div>
       <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-5">
-      <div class="col">
+        <div class="col">
           <div class="card shadow-md">
             <img src={evalue} alt="full-stack e-commerce" height={300} width="100%" />
             <div class="card-body">
               <p class="card-text">Full-stack E-commerce website, with Cart, Favorites and Login functionality.</p>
               <div class="d-flex justify-content-between align-items-center">
-              <div className="btn-group">
+                <div className="btn-group">
                   <a href="https://e-value-37653.web.app/" target="_blank" className="btn btn-sm btn-outline-secondary">Go to project</a>
                 </div>
                 <div className="btn-group">
@@ -51,7 +51,7 @@ const Projects = () => {
             <div class="card-body">
               <p class="card-text">Product listing e-commerce (without Cart, Favorites, Login functionality)</p>
               <div class="d-flex justify-content-between align-items-center">
-              <div className="btn-group">
+                <div className="btn-group">
                   <a href="https://product-listing-c4u.web.app/women/show%20all" target="_blank" className="btn btn-sm btn-outline-secondary">Go to project</a>
                 </div>
                 <div className="btn-group">
@@ -67,7 +67,7 @@ const Projects = () => {
             <div class="card-body">
               <p class="card-text">Fitness Tracker App, my final project at Telerik Academy. (not responsive)</p>
               <div class="d-flex justify-content-between align-items-center">
-              <div className="btn-group">
+                <div className="btn-group">
                   <a href="https://energize-57e14.web.app" target="_blank" className="btn btn-sm btn-outline-secondary">Go to project</a>
                 </div>
                 <div className="btn-group">
@@ -83,7 +83,7 @@ const Projects = () => {
             <div class="card-body">
               <p class="card-text">Music app built with NextJS and Tailwind CSS. Stay tuned..</p>
               <div class="d-flex justify-content-between align-items-center">
-              <div className="btn-group">
+                <div className="btn-group">
                   <a href="https://your-project-url.com" target="_blank" className="btn btn-sm btn-outline-secondary">Go to project</a>
                 </div>
                 <div className="btn-group">
